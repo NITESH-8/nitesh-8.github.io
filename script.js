f6804f7d7e7e02fe62ca272436e6be8806f678a2
nitesh-8 <niteshrawat24102000@gmail.com>
2021-06-10 01:49:11 +0530

    change: JS send button

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let phone = document.getElementById("from_phone");
 let message = document.getElementById("from_message");
 
 let form_data = document.querySelector("form");
-let form_button = document.getElementsByClassName("btn");
+let form_button = document.getElementsByClassName("submit-button");
 
 form_data.addEventListener("submit", function (e) {
   e.preventDefault();
@@ -16,8 +16,27 @@ form_data.addEventListener("submit", function (e) {
     from_phone: phone.value,
     message: message.value,
   };
-  // console.log(obj);
+  console.log(form_button);
+  form_button[0].classList.add("spin-button");
+  form_button[0].innerHTML = "<div class='spin'> </div>";
+
   emailjs.send("service_fw2vgww", "template_majyzek", obj).then((res) => {
     console.log(res.status);
+    // form_button[0].classList.remove("spin-button");
+
+    if (res.status == 200) {
+      form_button[0].innerHTML = "<div class='tick'> </div>";
+      // form_button[0].innerHTML = "Sent";
+      setTimeout(function () {
+        form_button[0].innerHTML = "SEND";
+        // alert("message Send successfully");
+        form_button[0].classList.remove("spin-button");
+      }, 3500);
+
+      // alert("Sent");
+    } else {
+      alert("Text not sent");
+      form_button[0].classList.remove("spin-button");
+    }
   });
 });
